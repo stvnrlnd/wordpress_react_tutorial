ee7c7df562b4803ac74e00201634d8b100f9eac7
Steven Roland <steven@mojave.io>
2020-08-16 11:12:22 -0400

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => (
+  <div>
+    <section className="section container content">
+      <h1>Hello Beautiful!</h1>
+    </section>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
