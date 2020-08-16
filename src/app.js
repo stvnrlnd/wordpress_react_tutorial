998ba944e90d11d4aaca512d9a121e053e5433eb
Steven Roland <steven@mojave.io>
2020-08-16 13:50:36 -0400

    Add routes to app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PostList from "./components/postList";
+import PostView from "./components/postView";
 
 const App = () => (
     <Router>
         <div>
             <section className="section container content">
                 <Route exact path="/" component={PostList} />
+                <Route path="/post" component={PostView} />
             </section>
         </div>
     </Router>
