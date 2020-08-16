4588a47e487ecc77edc02fe9ed1ca74f0aff01e5
Steven Roland <steven@mojave.io>
2020-08-16 11:13:09 -0400

    Add react-router

---
+++ src/components/postList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class PostList extends Component {
+    render() {
+        return (
+            <h1>Hello Gorgeous!</h1>
+        );
+    }
+}
+  
+export default PostList;
