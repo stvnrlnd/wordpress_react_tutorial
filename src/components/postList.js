e5a60e35d839731ce34da2399d51b3e0eb160742
Steven Roland <steven@mojave.io>
2020-08-17 14:40:31 -0400

    Add axios and flesh out the PostList component

--- src/components/postList.js
+++ src/components/postList.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class PostList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+        
+        this.htmlMarkup = this.htmlMarkup.bind();
+    }
+
+    componentDidMount() {
+        axios
+            .get("https://techcrunch.com/wp-json/wp/v2/posts")
+            .then(posts => {
+                this.setState({
+                    posts: posts.data
+                });
+            });
+    }
+
+    htmlMarkup(html) {
+        return { __html: html };
+    }
+
     render() {
         return (
-            <h1>Hello Gorgeous!</h1>
+            <div>
+                {this.state.posts.map(post => (
+                    <div className="card" key={post.id}>
+                        <div className="card-content">
+                            <h3>{post.title.rendered}</h3>
+                            <div dangerouslySetInnerHTML={this.htmlMarkup( post.excerpt.rendered )} />
+                        </div>
+                    </div>
+                ))}
+            </div>
         );
     }
 }
