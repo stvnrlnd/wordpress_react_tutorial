49e2ce5fcdb8cc9f2729518c1cf261b29b51ab0b
Steven Roland <steven@mojave.io>
2020-08-18 19:18:20 -0400

    Add main navigation component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navigation from "./components/navigation";
 import PostList from "./components/postList";
 import PostView from "./components/postView";
 
 const App = () => (
     <Router>
         <div>
+            <Navigation />
             <section className="section container content">
                 <Route exact path="/" component={PostList} />
                 <Route path="/:slug" component={PostView} />
