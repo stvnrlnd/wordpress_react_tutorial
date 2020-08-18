32217b1b374e5b76a87a9a62d35bd2cbd2f0291d
Steven Roland <steven@mojave.io>
2020-08-18 14:08:00 -0400

    Update PostView component

--- src/components/postView.js
+++ src/components/postView.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class PostView extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            post: {}
+        };
+
+        this.htmlMarkup = this.htmlMarkup.bind();
+    }
+
+    componentDidMount() {
+        const slug = this.props.match.params.slug;
+
+        axios
+            .get(`https://techcrunch.com/wp-json/wp/v2/posts?slug=${slug}`)
+            .then(post => {
+                this.setState({
+                    post: post.data[0]
+                });
+            });
+    }
+
+    htmlMarkup(html) {
+        return { __html: html };
+    }
+
     render() {
-        return (
-            <h1>Hello World!</h1>
-        );
+        let build;
+
+        if (this.state.post.title) {
+            build = (
+                <div>
+                    <h1>{this.state.post.title.rendered}</h1>
+                    <div dangerouslySetInnerHTML={this.htmlMarkup( this.state.post.content.rendered )} />
+                </div>
+            );
+        } else {
+            build = <div />;
+        }
+
+        return build;
     }
 }
   
